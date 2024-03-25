f3fe8c18af4960419b82331d7c5ccbb38dc7bcdb
Frederico27 <fredericomarcal27@gmail.com>
2024-03-25 16:31:42 +0900

    add resources assets to vercel

--- resources/views/login/register.blade.php
+++ resources/views/login/register.blade.php
@@ -7,7 +7,7 @@
 <div class="container">
     <div class="row justify-content-center mt-5">
         <div class="col-lg-4">
-            <div class="card">
+            <div class="card" style="margin-top: 40%">
                 <div class="card-header">
                     <h1 class="card-title">Register</h1>
                 </div>
